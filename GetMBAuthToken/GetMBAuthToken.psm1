b33011558f46cd9b645e258c890f64f3d33703dc
dmorriselc <127871488+dmorriselc@users.noreply.github.com>
2023-03-14 16:06:19 +0000

    uploading module

---
+++ GetMBAuthToken/GetMBAuthToken.psm1
@@ -0,0 +1,16 @@
+function GetMBAPIToken {
+	$clientID = "yourclientid"
+	$clientSecret = "yourclientsecret"
+	$encrypted_secret = "Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$($clientId):$clientSecret")))"
+
+	$TokenRequest = @{
+	  Uri     = 'https://auth.mythic-beasts.com/login'
+	  Method  = 'POST'
+	  Headers = @{'Authorization' = $encrypted_secret}
+	  Body    = @{grant_type = 'client_credentials'}
+	}
+	$TokenRestResponse = Invoke-RestMethod @TokenRequest
+	$token = $TokenRestResponse.access_token
+	
+	return($token)
+}
