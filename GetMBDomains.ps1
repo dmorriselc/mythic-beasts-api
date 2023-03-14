ec96632d0cfb85bc813055f7e9be1d0d7bc2a8c7
dmorriselc <127871488+dmorriselc@users.noreply.github.com>
2023-03-14 16:12:13 +0000

    adding getmbdomains.ps1

---
+++ GetMBDomains.ps1
@@ -0,0 +1,29 @@
+<#	
+	.NOTES
+	===========================================================================
+	 Created on:   	13/03/2023
+	 Created by:   	David Morris
+	 Organization: 	East Lothian Council
+	 Filename:     	GetMBDomains.ps1
+     Dependencies:  GetMBAuthToken module
+	===========================================================================
+	.DESCRIPTION
+    Example script, this will connect to the Mythic-Beasts API and retrieve a list of domains  
+#>
+
+#Requires -Modules GetMBAuthToken
+
+$Url = "https://api.mythic-beasts.com/dns/v2/zones"
+
+$token = GetMBAPIToken
+
+$ResponseParameters = @{
+	Uri = $Url
+	Method = 'GET'
+	Headers = @{'Authorization'="Bearer $token"}
+	ContentType = "application/json"
+}
+
+$APIResponse = Invoke-WebRequest @ResponseParameters
+
+Write-Output $APIResponse.Content | ConvertFrom-Json | ConvertTo-Json
